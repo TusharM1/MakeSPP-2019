b2270b330e9844eee500c8505be1c77267d2f6b0
Sahaj Patel <patelsahajemail@gmail.com>
2019-04-06 12:45:04 -0400

    Merge branch 'WifiP2P' of https://github.com/TusharM1/MakeSPP-2019 into WifiP2P
    # Conflicts:
    #	app/src/main/java/com/example/makespp_2019/MainActivity.java

---
+++ app/src/main/java/com/example/makespp_2019/ReceiveMessage.java
@@ -0,0 +1,13 @@
+package com.example.makespp_2019;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReceiveMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+    }
+}
