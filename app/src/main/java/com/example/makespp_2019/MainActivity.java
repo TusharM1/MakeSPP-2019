86bee4f69d726c762f8034485ebc529591ca55bb
Sahaj Patel <patelsahajemail@gmail.com>
2019-04-06 10:48:09 -0400

    Merge branch 'WifiP2P' of https://github.com/TusharM1/MakeSPP-2019 into WifiP2P
    # Conflicts:
    #	app/src/main/java/com/example/makespp_2019/MainActivity.java

--- app/src/main/java/com/example/makespp_2019/MainActivity.java
+++ app/src/main/java/com/example/makespp_2019/MainActivity.java
@@ -2,31 +2,80 @@
 package com.example.makespp_2019;
 
 import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
-    public static final String TAG = "MainActivity";
-    WifiP2pManager mManager;
-    WifiP2pManager.Channel mChannel;
-    BroadcastReceiver mReceiver;
-    public IntentFilter mIntentFilter;
+    private final IntentFilter intentFilter = new IntentFilter();
+    WifiP2pManager.Channel channel;
+    WifiP2pManager manager;
+    BroadcastReceiver receiver;
 
-//adfadsfasdfa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mManager = (WifiP2pManager) getSystemService(this.WIFI_P2P_SERVICE);
-        mChannel = mManager.initialize(this, getMainLooper(), null);
-        mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
-        mIntentFilter = new IntentFilter();
-        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION); //Check for a change in the status
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION); //Check for list of avaliable peers
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION); //CHeck for state of connectivity
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION); //Check for this device's details
+
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+                    // Determine if Wifi P2P mode is enabled or not, alert
+                    // the Activity.
+                    int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+                    if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                        activity.setIsWifiP2pEnabled(true);
+                    } else {
+                        activity.setIsWifiP2pEnabled(false);
+                    }
+                } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+
+                    // The peer list has changed! We should probably do something about
+                    // that.
+
+                } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+
+                    // Connection state changed! We should probably do something about
+                    // that.
+
+                } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+                    DeviceListFragment fragment = (DeviceListFragment) activity.getFragmentManager()
+                            .findFragmentById(R.id.frag_list);
+                    fragment.updateThisDevice((WifiP2pDevice) intent.getParcelableExtra(
+                            WifiP2pManager.EXTRA_WIFI_P2P_DEVICE));
+
+                }
+            }
+
+
+        };
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        receiver = new WiFiDirectBroadcastReceiver(manager, channel, this);
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
     }
 }
