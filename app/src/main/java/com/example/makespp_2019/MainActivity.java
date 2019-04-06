6b4ea00209842eeae020f4e209b706e9daaa7038
Sahaj Patel <patelsahajemail@gmail.com>
2019-04-06 14:12:04 -0400

    Finished GUI

--- app/src/main/java/com/example/makespp_2019/MainActivity.java
+++ app/src/main/java/com/example/makespp_2019/MainActivity.java
@@ -1,117 +1,30 @@
 package com.example.makespp_2019;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
-import android.widget.EditText;
-
-import java.util.TreeMap;
-
-import static java.lang.Thread.sleep;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity{
 
-	Button button;
-	EditText input;
-	Vibrator vibrator;
+	ImageButton toSend, toReceive;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+		toSend = findViewById(R.id.sendButton);
+		toReceive = findViewById(R.id.receiveButton);
 
-		button.setOnClickListener(v -> {
-			String morseCode = MorseCodeTranslator.plainTextToMorse(input.getText().toString());
-			for (char letter : morseCode.toCharArray())
-				switch (letter) {
-					case '.':
-						vibrator.vibrate(100);
-						break;
-					case '-':
-						vibrator.vibrate(300);
-						break;
-					case ' ':
-						try {
-							sleep(600);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-						break;
-				}
+		toSend.setOnClickListener(v -> {
+			Intent intent = new Intent(MainActivity.this, SendMessage.class);
+			startActivity(intent);
 		});
 
+		toReceive.setOnClickListener(v -> {
+			Intent intent = new Intent(MainActivity.this, ReceiveMessage.class);
+			startActivity(intent);
+		});
 	}
-
-	private static class MorseCodeTranslator {
-
-		private static TreeMap<String, String> morseCodeDictionary = new TreeMap<>();
-
-		private static final String[] keys = {
-				"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-				"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-				"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", " "
-		};
-
-		private static final String[] values = {
-				". -  ", 			// A
-				"- . . .  ",		// B
-				"- . - .  ",		// C
-				"- . .  ",			// D
-				".  ",				// E
-				". . - .  ",		// F
-				"- - .  ",			// G
-				". . . .  ",		// H
-				". .  ",			// I
-				". - - -  ",		// J
-				"- . -  ",			// K
-				". - . .  ",		// L
-				"- -  ",			// M
-				"- .  ",			// N
-				"- - -  ",			// O
-				". - - .  ",		// P
-				"- - . -  ",		// Q
-				". - .  ", 			// R
-				". . .  ", 			// S
-				"-  ",			    // T
-				". . -  ",			// U
-				". . . -  ",		// V
-				". - -  ",			// W
-				"- . . -  ",		// X
-				"- . - -  ",		// Y
-				"- - . .  ",		// Z
-
-				". - - - -  ",		// 1
-				". . - - -  ",		// 2
-				". . . - -  ",		// 3
-				". . . . -  ",		// 4
-				". . . . .  ",		// 5
-				"- . . . .  ",		// 6
-				"- - . . .  ",		// 7
-				"- - - . .  ",		// 8
-				"- - - - .  ",		// 9
-				"- - - - -  ",		// 0
-				"  "				// Space
-		};
-
-		static {
-			for (int i = 0; i < keys.length; i++)
-				morseCodeDictionary.put(keys[i], values[i]);
-		}
-
-		private static String plainTextToMorse(String plaintext) {
-			String morseCode = plaintext;
-			try {
-				for (String s : morseCodeDictionary.keySet())
-					morseCode = morseCode.replace(s, morseCodeDictionary.get(s));
-			}
-			catch (Exception ignored) {}
-			return morseCode;
-		}
-
-	}
-
 }
--- app/src/main/java/com/example/makespp_2019/ReceiveMessage.java
+++ app/src/main/java/com/example/makespp_2019/ReceiveMessage.java
@@ -1,13 +1,24 @@
 package com.example.makespp_2019;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ReceiveMessage extends AppCompatActivity {
+
+    ImageButton backToMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
+
+        backToMain = findViewById(R.id.receiveHomeButton);
+        backToMain.setOnClickListener(v -> {
+            Intent intent = new Intent(ReceiveMessage.this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
